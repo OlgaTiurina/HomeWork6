c6ccc67029dfbc59719fb4239ea8b01277bab4c2
olga <bagira_to@mail.ru>
2022-09-09 19:49:26 +0300

    Решение задачи 43

---
+++ Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindIntersectionPoint()
+{
+    Console.Write("Введите b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+     double x = (b2 - b1) / (k1 - k2);
+     double y = k1 * x + b1;
+
+     if((k1 == k2) && (b1 == b2)) Console.WriteLine("Прямые совпадают");
+     else
+     {
+        if (k1==k2) Console.WriteLine("Прямые параллельны");
+        else Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+     }
+}
+FindIntersectionPoint();
